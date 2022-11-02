d347acf14ea1893da505d1cdc9cea07768510235
Leevon Levasseur <leevonlevasseur@gmail.com>
2022-11-02 15:43:33 -0700

    Day 22

--- src/alpha-nhl/merge-data.py
+++ src/alpha-nhl/merge-data.py
@@ -8,8 +8,8 @@ def readJSON(path):
     return json.loads(json_file.read())
 
 def main():
-  ep_player_data = readJSON(cwd + "/json/ep-player-data.json")
-  manual_player_data = readJSON(cwd + "/json/manual-player-data.json")
+  ep_player_data = readJSON(cwd + "/json/alpha-nhl/ep-player-data.json")
+  manual_player_data = readJSON(cwd + "/json/alpha-nhl/manual-player-data.json")
 
   for team in manual_player_data:
     players = manual_player_data[team]
@@ -22,13 +22,13 @@ def main():
         print("skipping (in merge) "+ name)
       
 
-  with open(cwd + '/json/merged-player-data.json', 'w') as json_file:
+  with open(cwd + '/json/alpha-nhl/merged-player-data.json', 'w') as json_file:
     json_file.write(json.dumps(ep_player_data, indent=4))
 
   print('''
-  Player data has been merged from /json/ep-player-data.json and /json/manual-player-data.json
-  into /json/merged-player-data.json
-  Run `python src/data-to-md.py` to update the ROSTERS.md file with this new data
+  Player data has been merged from /json/alpha-nhl/ep-player-data.json and /json/alpha-nhl/manual-player-data.json
+  into /json/alpha-nhl/merged-player-data.json
+  Run `python src/alpha-nhl/data-to-md.py` to update the ROSTERS.md file with this new data
   ''')
   
 
