3ed6e27344e126d1a0f78a733cc58596b03c4f29
Leevon Levasseur <leevonlevasseur@gmail.com>
2022-11-07 11:33:38 -0800

    Revert to inline md with correct links

--- src/alpha-nhl/parse-standings.py
+++ src/alpha-nhl/parse-standings.py
@@ -3,7 +3,7 @@ import os
 
 cwd = os.getcwd()
 
-ROSTERS_URL = "https://github.com/llevasseur/hockey-fantasy-league/blob/main/ROSTERS.md"
+ROSTERS_URL = "https://github.com/llevasseur/world-juniors-2022/blob/main/ROSTERS.md"
 STANDINGS_URL = "https://github.com/llevasseur/hockey-fantasy-league/blob/main/STANDINGS.md"
 
 NUMBER_OF_PLAYERS = 6
@@ -60,7 +60,7 @@ def main():
     README_md = open(cwd + '/README.md', 'w')
 
     README_md.write("# Hockey League Fantasy Draft\n### Alpha Version: Data parsed with BeautifulSoup from eliteprospects.com\nCasual Python3 project used by friends to keep track of NHL and WJC players' stats. Statistics of players drafted by participants are totaled to determine Scoreboard ranking and to determine the winner.\n## Scoreboard\n")
-    README_md.write(f"| User | <a href='{STANDINGS_URL}#user-content-goals'>G</a> | <a href='{STANDINGS_URL}#user-content-assists'>A</a> | <a href='{STANDINGS_URL}#user-content-shots-on-goal'>SOG</a> | <a href='{STANDINGS_URL}#user-content-penalties-in-minutes'>PIM</a> | <a href='{STANDINGS_URL}#user-content-plus--minus'>+/-</a> | <a href='{STANDINGS_URL}#user-content-time-played-in-minutes'>TPM</a> | <a href='{STANDINGS_URL}#user-content-save-percentage'>S%</a> | <a href='{STANDINGS_URL}#user-content-goals-against-average'>GAA</a> | Total |\n")
+    README_md.write(f"| User | [G]({STANDINGS_URL}#user-content-goals) | [A]({STANDINGS_URL}#user-content-assists) | [SOG]({STANDINGS_URL}#user-content-shots-on-goal) | [PIM]({STANDINGS_URL}#user-content-penalties-in-minutes) | [+/-]({STANDINGS_URL}#user-content-plus--minus) | [TPM]({STANDINGS_URL}#user-content-time-played-in-minutes) | [S%]({STANDINGS_URL}#user-content-save-percentage) | [GAA]({STANDINGS_URL}#user-content-goals-against-average) | Total |\n")
     README_md.write(
         f"| :--- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- |  -----: |\n")
 
@@ -70,7 +70,7 @@ def main():
         overall_points.items(), key=lambda item: sum(item[1]), reverse=True)}
 
     for user in overall_points:
-        README_md.write(f"| <a href='{ROSTERS_URL}#{user}'>{user}</a> | ")
+        README_md.write(f"| [{user}]({ROSTERS_URL}#{user}) | ")
         for p in overall_points[user]:
             README_md.write(f"{p} | ")
         README_md.write(f"{sum(overall_points[user])} |\n")
