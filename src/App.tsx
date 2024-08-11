75066026ad44736dfef6e66ecb7bc37a9365635a
Leevon Levasseur <leevonlevasseur@gmail.com>
2024-08-11 16:20:43 -0700

    connects to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./components/pages/HomePage/HomePage";
+import NotFoundPage from "./components/pages/NotFound/NotFound";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/api/:id" element={<HomePage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/pages/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+/**
+ * Not Found
+ */
+const NotFoundPage = () => {
+  return (
+    <div>
+      404 NO LUCK BUD
+    </div>
+  )
+}
+
+export default NotFoundPage
